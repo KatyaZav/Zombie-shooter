5dbb588b694bf9ca87e9ec8283c966be8dd88496
Katya Zavyalova <82714610+KatyaZav@users.noreply.github.com>
2024-06-12 16:31:20 +0500

    update poison

--- Assets/_Scripts/Game/Abilities/PoisionAbility.cs
+++ Assets/_Scripts/Game/Abilities/PoisionAbility.cs
@@ -3,10 +3,14 @@ using UnityEngine;
 public class PoisionAbility : BaseAbility
 {
     [SerializeField] Spawner _spawner;
+    [SerializeField] PlayerInventory _inv;
     [SerializeField] float _damage;
 
     protected override void OnClick()
     {
+        if (Random.Range(0, 10) <= 1)
+            _inv.Hit(1);
+
         _spawner.PoisonAllZombies(_damage, _timeActive);
         base.OnClick();
     }
