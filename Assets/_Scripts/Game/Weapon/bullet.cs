333c9fc6b5d69f986cb9c68c5f510ded3ecd7dc0
Baronne_Noire <party2015ru@mail.ru>
2024-12-19 21:35:59 +0500

    refactor bullet

--- Assets/_Scripts/Game/Weapon/bullet.cs
+++ Assets/_Scripts/Game/Weapon/bullet.cs
@@ -7,18 +7,31 @@ public class bullet : MonoBehaviour
     [SerializeField] Rigidbody _rb;
     [SerializeField] float _speed;
     [SerializeField] GameObject _sprite;
+    [SerializeField] float _destroyTime = 1;
 
     void Start()
     {
         _rb.AddForce(Vector3.forward*_speed*2, ForceMode.VelocityChange);
     }
 
-    private void OnDestroy()
+    private void OnDestroed(Transform enemy)
     {
         if (transform.position.z <= 29)
         {
-            var o = Instantiate(_sprite, transform.position, transform.rotation);
-            Destroy(o);
+            var o = Instantiate(_sprite, transform.position, transform.rotation, enemy.parent);
+            Destroy(o, _destroyTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        IDamageble damagable = other.GetComponent<IDamageble>();
+
+        if (damagable != null)
+        {
+            damagable.TakeDamage(PlayerInventory.Damage);
+            OnDestroed(other.transform);
+            Destroy(gameObject);
         }
     }
 }
