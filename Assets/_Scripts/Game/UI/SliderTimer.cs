034e667dffe972276338e61e13b25945b27e0df2
Baronne_Noire <party2015ru@mail.ru>
2024-12-23 09:58:48 +0500

    fix bugs

--- Assets/_Scripts/Game/UI/SliderTimer.cs
+++ Assets/_Scripts/Game/UI/SliderTimer.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System;
 
 public class SliderTimer : MonoBehaviour
 {
     [SerializeField] Image _image;
     private float _timer;
     private float _currentTimer;
+
+    private bool _isPlaying = true;
 
     public void SetTime(float time)
     {
@@ -17,23 +20,36 @@ public class SliderTimer : MonoBehaviour
     {
         _currentTimer = _timer;
         StartCoroutine(TimerLogic());
+
+        Subscriber.ChangePauseEvent += ChangePause;
     }
 
     private IEnumerator TimerLogic()
     {
         while (_currentTimer >= 0)
         {
-           /* yield return new WaitForSeconds(1);
-            _image.fillAmount = Mathf.Lerp(_image.fillAmount, (_currentTimer-1)/_timer, 1);
-            _currentTimer--;*/
-            
-            _currentTimer -= Time.deltaTime;
-            float normalizedTime = Mathf.Clamp01(_currentTimer / _timer);
-            _image.fillAmount = normalizedTime;
-            yield return new WaitForFixedUpdate();
+            if (_isPlaying == false)
+                yield return null;
+            else
+            {            
+                _currentTimer -= Time.deltaTime;
+                float normalizedTime = Mathf.Clamp01(_currentTimer / _timer);
+                _image.fillAmount = normalizedTime;
+                yield return null;
+            }
         }
 
         gameObject.SetActive(false);
+    }
+
+    private void OnDestroy()
+    {
+        Subscriber.ChangePauseEvent -= ChangePause;
+    }
+
+    private void ChangePause(bool isPause)
+    {
+        _isPlaying = isPause == false;
     }
 
     private void OnValidate()
